3d067f4f9504e2c480b4fbcd4ac4db7d0dbb87c3
Noscka <Adam882200@gmail.com>
2022-09-30 14:51:52 +0100

    Fixed program skipping lines of code

--- DataCreater/DataCreater.cpp
+++ DataCreater/DataCreater.cpp
@@ -12,63 +12,50 @@
 
 void CreateRandomData(LoadingScreen* Object)
 {
-    try
+    INT64 OutputFileSize = (3.6 * Gigabyte);
+    INT64 SectionSize = (500 * Megabyte);
+
+    INT64 Progress = 0;
+    INT64 OutOf = OutputFileSize;
+
+    /* Create random data in txt file to send over network */
+    std::ofstream OutFileStream("RandomData.txt", std::ios::binary);
+
+    /* Calculate amount of times that the program will create 500MB section, write and repeat to save memory */
+    int FullOperationAmount = 0;
+    FullOperationAmount = (int)(OutputFileSize / SectionSize);
+
+
+    /* Calculate amount of data left which will be written exact (if 1.2GB sized file, then 2 500MB sections and then 200MB section for the remainder) */
+    INT64 LeftOverAmount = 0;
+    LeftOverAmount = OutputFileSize % SectionSize;
+
+    /* foreach loop for all the sections */
+    for(int i = 0; i < FullOperationAmount; i++)
     {
-        long OutputFileSize = 0;
-        //OutputFileSize = (3.6 * Gigabyte);
-        long SectionSize = 0;
-        //SectionSize = (500 * Megabyte);
-
-        long Progress = 0;
-        long OutOf = OutputFileSize;
-
-        /* Create random data in txt file to send over network */
-        std::ofstream OutFileStream("RandomData.txt", std::ios::binary);
-
-        wprintf(L"here");
-        /* Calculate amount of times that the program will create 500MB section, write and repeat to save memory */
-        long FullOperationAmount = 0;
-        FullOperationAmount = OutputFileSize / SectionSize;
-
-
-        /* Calculate amount of data left which will be written exact (if 1.2GB sized file, then 2 500MB sections and then 200MB section for the remainder) */
-        long LeftOverAmount = 0;
-        LeftOverAmount = OutputFileSize % SectionSize;
-
-
-        wprintf(L"here");
-
-        /* foreach loop for all the sections */
-        for(int i = 0; i < FullOperationAmount; i++)
+        std::string DataWrite;
+        for(int i = 0; i <= SectionSize; i += 10)
         {
-            std::string DataWrite;
-            for(int i = 0; i <= SectionSize; i += 10)
-            {
-                DataWrite += "aaaaaaaaaa";
-                Progress += 10;
-                Object->UpdateKnownProgressBar((float)Progress / (float)OutOf);
-            }
-            OutFileStream.write(DataWrite.c_str(), DataWrite.size());
+            DataWrite += "aaaaaaaaaa";
+            Progress += 10;
+            Object->UpdateKnownProgressBar((float)Progress / (float)OutOf);
         }
-
-        /* Remaining data */
-        {
-            std::string DataWrite;
-            for(int i = 0; i <= LeftOverAmount; i++)
-            {
-                DataWrite += "a";
-                Progress += 1;
-                Object->UpdateKnownProgressBar((float)Progress / (float)OutOf);
-            }
-            OutFileStream.write(DataWrite.c_str(), DataWrite.size());
-        }
-
-        OutFileStream.close();
+        OutFileStream.write(DataWrite.c_str(), DataWrite.size());
     }
-    catch(std::exception& e)
+
+    /* Remaining data */
     {
-        std::cerr << e.what() << std::endl;
+        std::string DataWrite;
+        for(int i = 0; i <= LeftOverAmount; i++)
+        {
+            DataWrite += "a";
+            Progress += 1;
+            Object->UpdateKnownProgressBar((float)Progress / (float)OutOf);
+        }
+        OutFileStream.write(DataWrite.c_str(), DataWrite.size());
     }
+
+    OutFileStream.close();
 }
 
 int main()
