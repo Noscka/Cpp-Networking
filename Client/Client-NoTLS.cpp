180ad197052cb541159897114e8efc85cc52e9f7
Noscka <Adam882200@gmail.com>
2022-11-06 20:49:11 +0000

    made 2 versions of both client and server (one with tls and one without) to dementrate with wireshark how the message can be seen

---
+++ Client/Client-NoTLS.cpp
@@ -0,0 +1,58 @@
+
+#include <SharedClass.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
+#include <windows.h>
+
+#include <iostream>
+#include <fstream>
+#include <io.h>
+#include <fcntl.h>
+
+
+/* https://dens.website/tutorials/cpp-asio/ssl-tls */
+
+int main()
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+
+    boost::asio::io_context io_context;
+
+    boost::asio::ip::tcp::socket socket(io_context);
+
+    try
+    {
+        wprintf(L"Type in hostname: ");
+        std::string HostName;
+        std::getline(std::cin, HostName);
+        if (HostName.empty())
+            HostName = "localhost";
+
+        /*
+        Connects to the function using `resolver` which resolves the address e.g. (Noscka.com -> 123.123.123.123)
+        Host - Hostname/Ip address
+        Service - Service(Hostname for ports)/Port number
+        */
+        boost::asio::connect(socket, boost::asio::ip::tcp::resolver(io_context).resolve(HostName, "58233"));
+
+        wprintf(L"Connected to server\n");
+
+        boost::system::error_code error;
+        std::string message;
+
+        wprintf(L"type a message: ");
+        std::getline(std::cin, message);
+        message += "\n";
+
+        boost::asio::write(socket, boost::asio::buffer(message), error);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    wprintf(L"Press any button to continue"); getchar();
+    return 0;
+}
---
+++ Server/Server-NoTLS.cpp
@@ -0,0 +1,88 @@
+#include <SharedClass.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/thread.hpp>
+#include <boost/array.hpp>
+
+#include <NosStdLib/String.hpp>
+
+#include <Windows.h>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <format>
+#include <io.h>
+#include <fcntl.h>
+#include <filesystem>
+#include <format>
+
+
+class tcp_connection
+{
+private:
+    boost::asio::ip::tcp::socket socket;
+
+    tcp_connection(boost::asio::io_context& io_context) : socket(io_context) {}
+public:
+    static tcp_connection* create(boost::asio::io_context& io_context) { return new tcp_connection(io_context); }
+
+    boost::asio::ip::tcp::socket& ConSocket() { return socket; }
+
+    void start()
+    {
+        wprintf(std::format(L"Client Connected from {}\n", GlobalFunction::ReturnAddress(socket.local_endpoint())).c_str());
+
+        try
+        {
+            boost::system::error_code error;
+
+            boost::array<char, 128> buf;
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            std::string StreamOutput(buf.data(), len);
+
+            wprintf(NosStdLib::String::ToWstring(StreamOutput).c_str());
+
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+
+    try
+    {
+        boost::asio::io_context io_context;
+        boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 58233));
+
+        SetConsoleTitle(std::wstring(L"File Server at " + GlobalFunction::ReturnAddress(acceptor.local_endpoint())).c_str());
+
+        wprintf(L"Server started\n");
+
+        while (true)
+        {
+            /* tcp_connection object which allows for managed of multiple users */
+            tcp_connection* newConSim = tcp_connection::create(io_context);
+
+            boost::system::error_code error;
+
+            /* accept incoming connection and assigned it to the tcp_connection object socket */
+            acceptor.accept(newConSim->ConSocket(), error);
+
+            /* if no errors, create thread for the new connection */
+            if (!error) { boost::thread* ClientThread = new boost::thread(boost::bind(&tcp_connection::start, newConSim)); }
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
